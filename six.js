2e5cf0e33f3061e1e426cf91c74386544c9d4d93
Gourav1221 <gouravmisra111@gmail.com>
2022-03-19 21:24:12 +0530

    update six

--- six.js
+++ six.js
@@ -8,3 +8,15 @@ const arr = [
 
 let op = [].concat(...arr)
 console.log('op:', op)
+
+console.log(arr.flat())
+
+
+const arr2 = [
+    [1,2],
+    [3,4],
+    [5,6,7,8],
+    [8,9,10,11,[12,13,14]]
+]
+
+console.log(arr2.flat(2))
